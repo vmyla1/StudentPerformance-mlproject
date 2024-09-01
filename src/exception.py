b5dd2833f001d79691bde2175bac7e5d596156aa
vmyla1 <147573339+vmyla1@users.noreply.github.com>
2024-09-01 19:06:42 -0400

    added EDA

--- src/exception.py
+++ src/exception.py
@@ -20,5 +20,16 @@ class CustomException(Exception):
         return self.error_message
     
 
+# if __name__ == "__main__":
+
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide the zero")
+#         raise CustomException(e, sys)
+
+     
+    
+
 
         
